0c55b9b71e65f853dd67f9ee23a45175c68ebe11
rdyyak <rdyyak@administrator-Inspiron-3521>
2015-01-23 16:37:52 +0200

    Changed properties fetching for repository configuration. Added resource filtering.

--- learning-project-dao/src/main/java/org/learning/project/dao/config/RepositoryContext.java
+++ learning-project-dao/src/main/java/org/learning/project/dao/config/RepositoryContext.java
@@ -2,15 +2,15 @@ package org.learning.project.dao.config;
 
 import java.util.Properties;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import org.hibernate.ejb.HibernatePersistence;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -18,33 +18,41 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 @Configuration
 @ComponentScan(basePackages = { "org.learning.project.dao" })
-@PropertySource("classpath:application.properties")
 @EnableJpaRepositories(basePackages = { "org.learning.project.dao" })
 public class RepositoryContext {
-
-	private static final String PROPERTY_NAME_DATABASE_DRIVER = "jdbc.driverClassName";
-	private static final String PROPERTY_NAME_DATABASE_URL = "jdbc.databaseurl";
-	private static final String PROPERTY_NAME_DATABASE_USERNAME = "jdbc.username";
-	private static final String PROPERTY_NAME_DATABASE_PASSWORD = "jdbc.password";
 
 	private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
 	private static final String PROPERTY_NAME_HIBERNATE_FORMAT_SQL = "hibernate.format_sql";
 	private static final String PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY = "hibernate.ejb.naming_strategy";
 	private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
 	private static final String PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
-	private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
 
-	@Resource
-	private Environment environment;
+	private @Value("${jdbc.driverClassName}") String driver ;
+	private @Value("${jdbc.databaseurl}") String databaseUrl;
+	private @Value("${jdbc.username}") String username;
+	private @Value("${jdbc.password}") String password;
 
+	private @Value("${hibernate.dialect}") String hibrnateDialect;
+	private @Value("${hibernate.format_sql}") String formatSql;
+	private @Value("${hibernate.ejb.naming_strategy}") String namingStrategy;
+	private @Value("${hibernate.show_sql}") String showSQL = "hibernate.show_sql";
+	private @Value("${hibernate.hbm2ddl.auto}") String hbm2dllAuto = "hibernate.hbm2ddl.auto";
+	private @Value("${entitymanager.packages.to.scan}") String packagesToScan = "entitymanager.packages.to.scan";
+
+	@Bean
+	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+		PropertyPlaceholderConfigurer placeholderConfigurer = new PropertyPlaceholderConfigurer();
+		placeholderConfigurer.setLocations(new ClassPathResource("application.properties"));
+		return placeholderConfigurer;
+	}
 	@Bean
 	public DataSource dataSource() {
 
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName(environment.getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER));
-		dataSource.setUrl(environment.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
-		dataSource.setUsername(environment.getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME));
-		dataSource.setPassword(environment.getRequiredProperty(PROPERTY_NAME_DATABASE_PASSWORD));
+		dataSource.setDriverClassName(driver);
+		dataSource.setUrl(databaseUrl);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
 		return dataSource;
 	}
 
@@ -62,19 +70,20 @@ public class RepositoryContext {
 		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
 
 		entityManagerFactoryBean.setDataSource(dataSource());
-		entityManagerFactoryBean.setPackagesToScan(environment.getRequiredProperty(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN));
+		entityManagerFactoryBean.setPackagesToScan(packagesToScan);
 		entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence.class);
 
 		Properties jpaProterties = new Properties();
-		jpaProterties.put(PROPERTY_NAME_HIBERNATE_DIALECT, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
-		jpaProterties.put(PROPERTY_NAME_HIBERNATE_FORMAT_SQL, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_FORMAT_SQL));
-		jpaProterties.put(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY));
-		jpaProterties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
-		jpaProterties.put(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO));
+		jpaProterties.put(PROPERTY_NAME_HIBERNATE_DIALECT, hibrnateDialect);
+		jpaProterties.put(PROPERTY_NAME_HIBERNATE_FORMAT_SQL, formatSql);
+		jpaProterties.put(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY, namingStrategy);
+		jpaProterties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, showSQL);
+		jpaProterties.put(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO, hbm2dllAuto);
 
 		entityManagerFactoryBean.setJpaProperties(jpaProterties);
 
 		return entityManagerFactoryBean;
 	}
+
 
 }
